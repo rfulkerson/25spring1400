6431c6e3e450a095d089cdf55c03bcb3e8d88da1
Robert A. Fulkerson <rfulkerson@unomaha.edu>
2025-02-19 16:02:02 -0600

    Adding Lecture 09

---
+++ Lecture_09/Q5.py
@@ -0,0 +1,12 @@
+x = int(input("enter value for x: "))
+
+# 210
+
+if (x < 110) or (x > 100):
+# if x > 100 and x < 110:
+# if 100 < x < 110:
+    print('One')
+elif (x >= 200) and (x <= 210):
+    print('Two')
+else:
+    print('Three')
