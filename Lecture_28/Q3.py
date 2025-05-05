a4ce4aa65090df1d04694338e9dba6b5081e6c98
Robert Fulkerson <rfulkerson@unomaha.edu>
2025-05-05 15:30:47 -0500

    Updated files

--- Lecture_28/Q3.py
+++ Lecture_28/Q3.py
@@ -1,5 +1,5 @@
 import csv
-values = [[7,4],[27,1,9],[1,2,3,4,5,6,7,8,9,10],[9]
+values = [[7,4],[27,1,9],[1,2,3,4,5,6,7,8,9,10],[9]]
 with open('newcsv.csv', 'w') as myfile:
     csv_writer = csv.writer(myfile)
     for i in values:
